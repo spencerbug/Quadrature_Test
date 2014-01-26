353cb57a7ddff168ab0b71ca0886e991f58a7c57
spencerbug <spencer.neilan@gmail.com>
2014-01-26 15:37:57 -0600

    Initial Commit
    Tests Quadrature Subsystem by I2C

---
+++ Quadrature_Test.ino
@@ -0,0 +1,13 @@
+//Spencer Neilan, 2014
+//Senior Design Project Powder Horn, Team221 Robotics LLC
+//This program tests the Quadrature Subsystem, by interfacing to the ATTiny45s via I2C.
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
