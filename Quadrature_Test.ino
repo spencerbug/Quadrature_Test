e2bbf3378967d53c5d0c894b82eca0fa4c5614dc
spencerbug <spencer.neilan@gmail.com>
2014-02-16 01:32:47 -0600

    Working Version
    Created function that uses micros() to measure speed in ticks per
    second,  each time it's called. getSpeed works for any individual
    encoder

--- Quadrature_Test.ino
+++ Quadrature_Test.ino
@@ -2,48 +2,79 @@
 //Spencer Neilan, 2014
 //Senior Design Project Powder Horn, Team221 Robotics LLC
 //This program tests the Quadrature Subsystem, by interfacing to the ATTiny45s via I2C.
-
+//modified  on 2/7/2014.
 #include <Wire.h>
 #include <TQED.h>
 
-TQED qed0(0x36);
-TQED qed1(0x37);
-unsigned long qed0time, qed1time;
-unsigned int qed0old, qed1old;
-unsigned int qed0new, qed1new;
-unsigned int speed0, speed1;
+
+unsigned long qed0time, qed1time;  //timestamp the speed values
+long qed0old, qed1old;    //
+long qed0new, qed1new;
+float speed0, speed1;
+
+  TQED qed0(byte(0x36));
+  TQED qed1(byte(0x37));
 
 void setup()
 {
-  qed0time=qed1time=millis();
   Serial.begin(115200);  // start serial for output
   Serial.println("TQED Test");
+
+  qed0time=qed1time=millis();
+  Wire.begin();
 }
+
+float getSpeed(TQED qed){
+  static unsigned long lastTime=0;
+  //units of ticks per second
+  float speed = 1000000.0*(float)((32767.0-qed.getCount())/(micros()-lastTime));
+  //bring the count back to 32767
+  qed.centerCount();
+  lastTime = micros();
+  return speed;
+}
+
 
 void loop()
 {
-  qed0new=qed0.getCount();
-  qed1new=qed1.getCount();
-  
-  //calculate the speed. This should be negative if reversing
-  speed0=(qed0new-qed0old)/(qed0time-millis());
-  speed1=(qed1new-qed1old)/(qed1time-millis());
-  
-  //Display the counter value
+//qed0.resetCount();
+  float speed0 = getSpeed(qed0);
+  uint16_t count=qed0.getCount();
   Serial.print("QED0: ");
-  Serial.print(qed0new);
-  Serial.print(" QED1: ");
-  Serial.print(qed1new);
-  //Display the speed
-  Serial.print(" speed0: ");
-  Serial.print(speed0);
-  Serial.print(" speed1: ");
-  Serial.print(speed1);
-  
+  Serial.println(speed0);
   delay(100);
-  qed0time=qed1time=millis();
-  qed0old=qed0new;
-  qed1old=qed1new;
+//  qed0new=qed0.getCount();
+//  qed1new=qed1.getCount();
+//  
+//  //calculate the speed. This should be negative if reversing
+//  //later, disregard this value if it's greater than 1000
+//  float speedold=speed0;
+//  speed0=((float)(qed0new-qed0old))/(millis()-qed0time);
+//  if(speed0>1000.0) speed0=speedold;
+//  speed1=((float)(qed1new-qed1old))/(millis()-qed1time);
+//  
+//  //Display the counter value
+//  Serial.println((float) (qed0new-qed0old));
+//  Serial.println(millis() - qed0time);
+//  delay(100);
+//  
+//  Serial.print("QED0: ");
+//  Serial.print(qed0new);
+//  //Serial.print(" QED1: ");
+//  //Serial.print(qed1new);
+//  //Display the speed
+//  Serial.print(" speed0: ");
+//  Serial.print(speed0);
+//  //Serial.print(" speed1: ");
+//  //Serial.print(speed1);
+//  Serial.println();
+//  
+//  qed0time=qed1time=millis();
+//  qed0old=qed0new;
+//  qed1old=qed1new;
+//  
+//  //when qed count gets closer to 
+//  
 }
 
 void ChangeAddress(TQED qed)
